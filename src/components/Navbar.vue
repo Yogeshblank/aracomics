566159bf42c968019f04ec18f25f33515f1d3ca4
Yogesh <yoyo31197@gmail.com>
2021-11-08 15:43:59 +0530

    added

---
+++ src/components/Navbar.vue
@@ -0,0 +1,39 @@
+<template>
+    <nav>
+    <v-app-bar app>
+      <div class="d-flex align-center">
+        <v-toolbar-title>
+        <span class="font-weight-light">Ara</span>
+        <span class="font-weight-medium">Komix</span>
+      </v-toolbar-title>
+      </div>
+    <v-spacer></v-spacer>
+      <v-btn icon @click="chgtheme()">
+        <v-icon>mdi-palette-swatch-variant</v-icon>
+      </v-btn>
+    </v-app-bar>
+     <v-snackbar v-model="snack" :timeout="800" top>
+      <span>{{the + " Theme Enabled"}}</span>
+    </v-snackbar>
+    </nav>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({ 
+    data() {
+    return {
+      drawer: null,
+      snack : null,
+      the:"",
+    };
+  },
+  methods: {
+    chgtheme() {
+      this.snack = true;
+      this.$store.dispatch("ctheme");
+      this.the = this.$store.state.theme;
+    }
+  }
+})
+</script>
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,40 @@
-import { createStore } from "vuex";
+import Vue from "vue";
+import Vuex from "vuex";
 
-export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    theme: "Light",
+    user: null
+  },
+  getters: {
+    chtheme: state => {
+      if (localStorage.theme) {
+        state.theme = localStorage.theme;
+      }
+    }
+  },
+  mutations: {
+    ctheme: state => {
+      if (state.theme == "Light") {
+        state.theme = "Dark";
+        localStorage.theme = state.theme;
+      } else {
+        state.theme = "Light";
+        localStorage.theme = state.theme;
+      }
+    },
+    updateUser(state, { user }) {
+      Vue.set(state, "user", user);
+    }
+  },
+  actions: {
+    ctheme: context => {
+      context.commit("ctheme");
+    },
+    updateUser: context => {
+      context.commit("updateUser");
+    }
+  }
 });
